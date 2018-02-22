5c22d432b22a1bfbbfbb15f3991d775ae8590206
alfredlima <alfredolimams@ic.ufal.br>
2018-02-22 17:31:26 -0300

    Reestruturação e configuração das rotas.

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alfredolima/Área de Trabalho/Test/Sistema-de-Biblioteca/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
